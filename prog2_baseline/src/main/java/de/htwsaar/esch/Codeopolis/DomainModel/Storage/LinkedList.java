bf7c1ad3cf6e7526ad146a180410a337b6bb13f9
huwano <akirpap03@gmail.com>
2024-05-16 23:26:55 +0200

    ex4 Nummer 1 Todo funktionalität testen und edge casses abdecken. Sonst fehlt noch der Iterator und die eingliederung in die Spielstruktur

--- prog2_baseline/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/Storage/LinkedList.java
+++ prog2_baseline/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/Storage/LinkedList.java
@@ -37,21 +37,77 @@ public class LinkedList <T> {
         size++;
     }
 
-    public boolean isEmpty(){
-        if(head == null){
-            return true;
-        }else {
-            return false;
+    public void removeFirst(){
+        if(this.head != null){
+            this.head = this.head.next;
+            size--;
         }
     }
 
-    public void addLast(){
-        if(!isEmpty()){
-
-        }
+    public boolean isEmpty(){
+        return this.head == null;
     }
 
     public int size(){
+        return this.size;
+    }
+
+    public T get (int index){
+        if(index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }else {
+            Node<T> temp = this.head;
+            for (int i = 0; i < index; i++) {
+                temp= temp.next;
+            }
+            return temp.content;
+        }
+    }
+
+    public T set(T content,int index){
+        if(index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }else {
+
+            //TODO checken ob es funktioniert
+            //idee ist: wen wir 2 ersetzten wollen gehen wir bis 1 speichen uns den content von 2 ab. +
+            //Dann erstellen wir einen neuen Knoten der anstelle von 2 sein wird.
+            //Darauf sagen wir den neuen Knoten das next = zwei weiter von 1 ist also 3.
+            // Nun müssen wir nur noch 1 sagen, dass newNode der next Knoten ist.
+            // Es folgt 1 NewNode 3 als Rheinfolge.
+            Node<T> temp = this.head;
+            for (int i = 0; i < index-1; i++) {
+                temp= temp.next;
+            }
+            T result = temp.next.content;
+            Node<T> newNode = new Node<>(content);
+            newNode.next = temp.next.next;
+            temp.next = newNode;
+            return result;
+        }
+    }
+
+    public void clear(){
+        this.head = null;
+    }
+
+    public T remove(int index){
+        if(index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }else {
+
+            //TODO checken ob es funktioniert vielleicht ist case beim head remove krittisch
+            Node<T> temp = this.head;
+            for (int i = 0; i < index-1; i++) {
+                temp= temp.next;
+            }
+            T result = temp.next.content;
+            temp.next = temp.next.next;
+            return result;
+        }
+    }
+
+    public int test(){
         int count = 0;
         if (isEmpty()){
             return count;
