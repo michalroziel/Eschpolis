888fae49855f6d183ddee23c5e6336bd52b94922
huwano <akirpap03@gmail.com>
2024-05-16 23:03:04 +0200

    ex4 Nummer 1

--- prog2_baseline/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/Storage/LinkedList.java
+++ prog2_baseline/src/main/java/de/htwsaar/esch/Codeopolis/DomainModel/Storage/LinkedList.java
@@ -5,9 +5,10 @@ import org.w3c.dom.Node;
 public class LinkedList <T> {
     // First Elm. of the List
     private Node<T> head;
+    private int size;
 
     // Innerclass Node
-    private class Node<T>{
+    public static class Node<T>{
         T content;
         Node<T> next;
         public Node(T content){
@@ -19,6 +20,21 @@ public class LinkedList <T> {
     //Constructor initialize with empty list head = null
     public LinkedList(){
         this.head = null;
+        this.size = 0;
+    }
+
+    public void addLast(T content){
+        Node<T> node = new Node<>(content);
+        if(this.head == null){
+            this.head = node;
+        }else {
+            Node<T> temp = this.head;
+            while(temp.next != null ){
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+        size++;
     }
 
     public boolean isEmpty(){
@@ -30,7 +46,25 @@ public class LinkedList <T> {
     }
 
     public void addLast(){
+        if(!isEmpty()){
 
+        }
+    }
+
+    public int size(){
+        int count = 0;
+        if (isEmpty()){
+            return count;
+        }
+        Node<T> temp = head;
+        while (true){
+            if (temp.next == null){
+                break;
+            }
+            temp = temp.next;
+            count ++;
+        }
+        return count;
     }
 
 }
