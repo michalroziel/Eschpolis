c6b10fc74d4a38fcf7ed66fd6748b5877dcbab34
huwano <akirpap03@gmail.com>
2024-05-17 13:57:01 +0200

    ex4 test datei erstell

---
+++ prog2_baseline/src/test/java/de/htwsaar/esch/Codeopolis/DomainModel/Storage/LinkedListTest.java
@@ -0,0 +1,44 @@
+package de.htwsaar.esch.Codeopolis.DomainModel.Storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void test1() {
+    }
+}
