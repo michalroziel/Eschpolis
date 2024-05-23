3658d323a6eb0d510f5a574d994d9e3b78bbab12
Michał Roziel <rozielmichal@gmail.com>
2024-05-23 14:38:06 +0200

    changed testEmptySilo Test to use an LinkedList

--- prog2_baseline/src/test/java/de/htwsaar/esch/codeopolis/tests/SiloTests.java
+++ prog2_baseline/src/test/java/de/htwsaar/esch/codeopolis/tests/SiloTests.java
@@ -2,6 +2,7 @@ package de.htwsaar.esch.codeopolis.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import de.htwsaar.esch.Codeopolis.DomainModel.Storage.LinkedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -50,9 +51,9 @@ public class SiloTests {
         silo.store(cornHarvest);
         silo.store(cornHarvest2);
 
-        Harvest[] removedHarvests = silo.emptySilo();
+        LinkedList<Harvest> removedHarvests = silo.emptySilo();
 
-        assertEquals(2, removedHarvests.length); // Two harvests removed
+        assertEquals(2, removedHarvests.size()); // Two harvests removed
         assertEquals(0, silo.getFillLevel()); // Silo empty
     }
 
